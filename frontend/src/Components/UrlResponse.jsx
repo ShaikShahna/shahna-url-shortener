fae77919efb463b25ff43f11ae21807da67a5fc6
Shaik Shahna <shaikshahna31@gmail.com>
2025-09-12 15:11:02 +0530

    urlshort update

--- frontend/src/Components/UrlResponse.jsx
+++ frontend/src/Components/UrlResponse.jsx
@@ -1,14 +1,55 @@
-import { Text } from '@mantine/core';
-import Service from '../utils/http'
+import { Text, Tooltip, Container, Group, ActionIcon } from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
+import { IconCheck, IconCopy } from '@tabler/icons-react';
+import { QRCodeSVG } from "qrcode.react";
+import Service from '../utils/http';
+
 const obj = new Service();
 
-
 export default function UrlResponse(props) {
-   const surl = obj.getBaseURL() + '/api/s/' + props?.response?.shortCode;
-   return (
-       <div>
-           <Text color="blue"> {surl} </Text>
-       </div>
-   )
-}
+  const clipboard = useClipboard({ timeout: 2000 });
+  const surl = obj.getBaseURL() + '/api/s/' + props?.response?.shortCode;
 
+  return (
+    <Container>
+      {/* URL + Copy icon side by side */}
+      <Group gap="xs">
+        <Text color="blue" fw={500} size="lg">
+          {surl}
+        </Text>
+        <Tooltip
+          label="Link copied!"
+          offset={5}
+          position="bottom"
+          radius="xl"
+          transitionProps={{ duration: 100, transition: 'slide-down' }}
+          opened={clipboard.copied}
+        >
+          <ActionIcon
+            variant="subtle"
+            color={clipboard.copied ? "teal" : "gray"}
+            onClick={() => clipboard.copy(surl)}
+          >
+            {clipboard.copied ? (
+              <IconCheck size={20} stroke={1.5} />
+            ) : (
+              <IconCopy size={20} stroke={1.5} />
+            )}
+          </ActionIcon>
+        </Tooltip>
+      </Group>
+
+      {/* QR code below */}
+      <QRCodeSVG
+        imageSettings={{
+          excavate: true,
+          src: '/HomeBackground.png',
+          height: 100,
+          width: 100,
+        }}
+        value={surl}
+        size={400}
+      />
+    </Container>
+  );
+}
